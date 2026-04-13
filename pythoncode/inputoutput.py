945b0a55681de355d39af2337ed765bdc71efb05
premkumar <premkumar.unix@gmail.com>
2026-04-13 08:59:18 +0530

    add basic python code

---
+++ pythoncode/inputoutput.py
@@ -0,0 +1,6 @@
+# in-built function  print() and input(), range(), len(), type(), int(), float(), str(), list(), tuple(), dict(), set()
+print("I am learning python")
+a = input("what is your name ?: ")
+print("My name is :", a)
+print(type(a))
+
